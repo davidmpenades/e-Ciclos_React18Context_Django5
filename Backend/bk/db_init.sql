2f31fb545ae783cb6c9dcac9e9a620e6c434e3a8
Davidmpenades <davidmpenades@gmail.com>
2024-02-19 00:12:06 +0100

    finish docker

---
+++ Backend/bk/db_init.sql
@@ -0,0 +1,12 @@
+-- Crear la base de datos
+CREATE DATABASE emove;
+
+-- Conectarse a la base de datos emove
+\c emove;
+
+-- Crear un usuario y asignarle privilegios
+-- CREATE USER david WITH PASSWORD '1234';
+ALTER ROLE david SET client_encoding TO 'utf8';
+ALTER ROLE david SET default_transaction_isolation TO 'read committed';
+ALTER ROLE david SET timezone TO 'UTC';
+GRANT ALL PRIVILEGES ON DATABASE emove TO david;
